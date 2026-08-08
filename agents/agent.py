e342b0cb8cddf031e0bf680faa1caa684d88ae6b
Prabash <mvsprabash@gmail.com>
2026-08-08 22:16:29 +0530

    feat: add state parameter to choose_action method in Agent

--- agents/agent.py
+++ agents/agent.py
@@ -1,8 +1,9 @@
 from abc import ABC, abstractmethod
 from game.action import Action
+from game.state import State
 
 class Agent(ABC):
     def __init__(self):...
 
     @abstractmethod
-    def choose_action(self) -> Action:...
+    def choose_action(self, state: State) -> Action:...
--- agents/random_agent.py
+++ agents/random_agent.py
@@ -1,11 +1,12 @@
 from agents.agent import Agent
 from game.action import Action
+from game.state import State
 import random
 
 class RandomAgent(Agent):
     def __init__(self):...
 
-    def choose_action(self) -> Action:
+    def choose_action(self, state: State) -> Action:
         row = random.randint(0, 2)
         col = random.randint(0, 2)
         return Action(row, col)
